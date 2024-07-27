bee1c4e1cf54dbec2aa26c936d4b4ffed21086fc
Nir Lanka <nir3al@gmail.com>
2024-07-27 20:39:57 +0800

    Switch to pipenv for package management

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-workdir=$(pwd)
-source $workdir/.venv/bin/activate
-
-python3 -m code
--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-workdir=$(pwd)
-python3 -m venv $workdir/.venv
-source $workdir/.venv/bin/activate
-
-python3 -m pip install xerox
-
-mkdir downloads
