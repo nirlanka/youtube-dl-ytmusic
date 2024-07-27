a091467cb4b9fbdb81b836cfc11663f05017f772
Nir Lanka <nir3al@gmail.com>
2024-07-27 19:50:13 +0800

    Add readme, import youtube_dl

---
+++ README.md
@@ -0,0 +1,3 @@
+# youtube-dl-ytmusic - wrapper for data hording
+
+Warning: Not a professional tool. I'll be iteratively updating and making this usable as I experiment command line tools and Python.
--- code.py
+++ code.py
@@ -1,11 +1,15 @@
 #!/opt/homebrew/bin/python3
 
+from __future__ import unicode_literals # for youtube_dl
+
 import xerox
+import subprocess
+
+import youtube_dl
 
 print("""NOTE:
     Copy the cookies in youtube.com or music.youtube.com in Netscape format.
-        (Use Cookie-Editor /free/ - https://cookie-editor.com)
-""")
+        (Use Cookie-Editor /free/ - https://cookie-editor.com)""")
 
 should_read_cookies = input('Read cookies from clipboard? (y/n): ')
 
@@ -14,4 +18,6 @@ if should_read_cookies == "y":
   file.write(xerox.paste())
   file.close()
 
+opts = {}
 
+#with youtube_dl.YoutubeDL
