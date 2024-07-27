12db920fb09e719bf01b1d08f15ef738deb2ff0f
Nir Lanka <nir3al@gmail.com>
2024-07-27 22:09:34 +0800

    Ad download code

--- code.py
+++ code.py
@@ -7,17 +7,52 @@ import subprocess
 
 import youtube_dl
 
-print("""NOTE:
-    Copy the cookies in youtube.com or music.youtube.com in Netscape format.
-        (Use Cookie-Editor /free/ - https://cookie-editor.com)""")
+import credentials
 
-should_read_cookies = input('Read cookies from clipboard? (y/n): ')
+print("""NOTE:
+  Copy the cookies in youtube.com or music.youtube.com in Netscape format.
+    (Use Cookie-Editor /free/ - https://cookie-editor.com)""")
+
+should_read_cookies = input("Read cookies from clipboard? (y/n): ")
 
 if should_read_cookies == "y":
-  file = open('cookies.txt', 'w')
+  file = open("cookies.txt", "w")
   file.write(xerox.paste())
   file.close()
 
-opts = {}
+opt = {
+  "cookies": "cookies.txt",
+  #"username": credentials.username,
+  #"password": credentials.password,
+  "format": "bestaudio/best",
+  "extract-audio": True,
+#  "audio-format": "mp3",
+  "audio-quality": 0,
+}
 
-#with youtube_dl.YoutubeDL
+urls = []
+url_inp = input("YouTube Music song URL: ")
+url_inp = url_inp.replace("music.", "www.")
+url_inp = url_inp.split("$")[0]
+urls = [url_inp]
+print()
+print("Downloading URLS (modified):")
+print(urls)
+
+print()
+print("Shell equivalent:")
+print("```")
+print("youtube-dl" \
+        + " --cookies cookies.txt" \
+        #+ " --username " + opt["username"] \
+        #+ " --password \"" + opt["password"] + "\""\
+        + " --format " + opt["format"] \
+        + " --extract-audio"  \
+        + " --audio-quality " + str(opt["audio-quality"]) \
+        + " " + urls[0] \
+      )
+print("```")
+print()
+
+with youtube_dl.YoutubeDL(opt) as ydl:
+    ydl.download(urls)
